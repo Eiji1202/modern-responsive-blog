3f90345d2e50bebf593d701df9851e004031134f
EIji Tachikawa <tsuyoku1202@gmail.com>
2024-07-07 14:09:53 +0900

    feat: WorkExperienceセクション作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { ProfileOverView } from "./components/ProfileOverView";
 import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
+import { WorkExperience } from "./components/WorkExperience";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <ProfileOverView />
       <Projects />
       <Skills />
+      <WorkExperience />
     </>
   );
 }
