4205b2fbc6acb322a83e5c133b08220994ce9260
EIji Tachikawa <tsuyoku1202@gmail.com>
2024-07-07 13:29:14 +0900

    feat: Skillsセクション作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { ProfileOverView } from "./components/ProfileOverView";
 import { Projects } from "./components/Projects";
+import { Skills } from "./components/Skills";
 
 export default function Home() {
   return (
     <>
       <ProfileOverView />
       <Projects />
+      <Skills />
     </>
   );
 }
